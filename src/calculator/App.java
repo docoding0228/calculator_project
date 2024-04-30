27c315c987094ad874a355593d7100e5cafc9017
docoding0228 <npng0228@naver.com>
2024-04-30 14:43:26 +0900

    Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.

---
+++ src/calculator/App.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
+
+    }
+}
